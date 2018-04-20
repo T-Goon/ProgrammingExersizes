0026ca117607cab22774cccb8ec249bdf7d5abd7
Timothy Goon <t.kgoonx@gmail.com>
2018-04-20 11:36:49 -0400

    Create EvenOrOdd.py

---
+++ EvenOrOdd/EvenOrOdd.py
@@ -0,0 +1,8 @@
+# prompt for and store a number
+num = int(input("Enter a number: "))
+
+# if the number is even say so, if not say that it is not
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
