6f45eceb9bf7190438727c71ac530fefd142a279
Timothy Goon <t.kgoonx@gmail.com>
2018-05-02 18:58:48 -0400

    Update HelloWorldAlt.py

--- PythonTkinter/HelloWorldAlt.py
+++ PythonTkinter/HelloWorldAlt.py
@@ -6,17 +6,19 @@ class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-
+        
+        # Exit Button
         self.button = Button(frame, text="Exit",
         fg= "blue", command = frame.quit)
         self.button.pack(side = RIGHT)
-
+        
+        # Prints 'hi!' to console 
         self.hiThere = Button(frame, text="HI", command = self.say_hi)
         self.hiThere.pack(side = LEFT)
 
     # Function that prints something
     def say_hi(self):
-        print("你好啊！")
+        print("hi！")
 
 # Create Tk object and pass it into the 'App' class
 root = Tk()
