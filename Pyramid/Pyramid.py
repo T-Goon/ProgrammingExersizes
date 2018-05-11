5ee6479793bb625761d30d10be2efca853373341
Timothy Goon <t.kgoonx@gmail.com>
2018-05-11 10:19:07 -0400

    Create Pyramid.py

---
+++ Pyramid/Pyramid.py
@@ -0,0 +1,18 @@
+def main():
+    height = int(input("How tall do you want the pyramid to be? "))
+
+    # Loop for each row of pyramid
+    for i in range(height):
+
+        # Loop for the spacing of each row
+        for x in range(i, height):
+            print(" ",end="")
+
+        # Loop to print the '*'s on each row
+        for y in range((i*2)+1):
+            print("*",end="")
+        # New line at the end of each row
+        print("")
+
+if __name__ == "__main__":
+    main()
