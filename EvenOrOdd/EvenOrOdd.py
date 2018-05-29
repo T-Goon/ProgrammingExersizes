1501d09b4d5f9c2dc5be3be522faf296acd5034c
Timothy Goon <t.kgoonx@gmail.com>
2018-05-29 16:32:53 -0400

    Update EvenOrOdd.py

--- EvenOrOdd/EvenOrOdd.py
+++ EvenOrOdd/EvenOrOdd.py
@@ -3,6 +3,7 @@ def EO (num):
 
 # if the number is even say so, if not say that it is not
 if EO(int(input("Enter a number: "))):
+    
     print("The number is even.")
 else:
     print("The number is odd.")
