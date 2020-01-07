7e7ff8713d3c8322485076202f45858a4570e14c
Timothy Goon <t.kgoonx@gmail.com>
2020-01-07 18:29:58 -0500

    Comments and code cleanup.

--- Neural_Networks/CartPole/cart_pole.py
+++ Neural_Networks/CartPole/cart_pole.py
@@ -12,6 +12,8 @@ MAX_EPS = 1
 MIN_EPS = .01
 
 class Model:
+    # num_states: number of items that make up the state in the envirnment
+    # num_actions: number of actions that are possible in the game
     def __init__(self, num_states, num_actions, batch_size):
         self._num_states = num_states
         self._num_actions = num_actions
@@ -20,18 +22,16 @@ class Model:
         self.defModel()
 
     def defModel(self):
+        # NN with 2 dense layers
         input = keras.layers.Input(shape=(self._num_states,))
         d1 = keras.layers.Dense(50, activation='relu')(input)
         d2 = keras.layers.Dense(20, activation='relu')(d1)
         d3 = keras.layers.Dense(self._num_actions)(d2)
+
         self.model = keras.Model(inputs=input, outputs=d3)
         self.model.compile(optimizer='Adam', loss='mse')
 
     def train_batch(self, train_x, train_y):
-        # dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y)
-        # ).shuffle(self.batch_size).repeat()
-
-        # self.model.fit(dataset, steps_per_epoch=1, verbose=0)
         self.model.fit(train_x, train_y, batch_size=self._batch_size, verbose=0)
 
     def predict(self, state):
@@ -52,17 +52,23 @@ class Model:
     def batch_size(self):
         return self._batch_size
 
+# Storage for previos states in the game
+# Used to train model
 class Memory:
+    # Init with a max number of samples that it can store
     def __init__(self, max_size):
         self.max_size = max_size
         self.data = list()
 
     def add_sample(self, sample):
         self.data.append(sample)
+        # Removes the oldest item if memory is at capacity
         if len(self.data) > self.max_size:
             self.data.pop(0)
 
     def get_samples(self, num_samples):
+        # Return num_samples number of random sample from memory
+        # if there are not enough samples in memory return what it has
         if len(self.data) < num_samples:
             return random.sample(self.data, len(self.data))
         else:
@@ -70,35 +76,35 @@ class Memory:
 
 class GameRunner:
     def __init__(self, model, env, memory, max_eps, min_eps, eps_decay_fac, render=True):
-        self.env = env
-        self.model = model
-        self.memory = memory
-        self.min_eps = min_eps
-        self.max_eps = max_eps
-        self.eps_decay_fac = eps_decay_fac
-        self.eps = self.max_eps
-        self.render = render
+        # Game envirnment, model, and memory classes
+        self._env = env
+        self._model = model
+        self._memory = memory
+
+        # Variables for epsilon greedy policy
+        self._min_eps = min_eps
+        self._max_eps = max_eps
+        self._eps_decay_fac = eps_decay_fac
+        self._eps = self._max_eps
+
+        self._render = render
         self._steps = 0
         self._best_run = 0
 
     def run(self):
-        state = env.reset()
+        state = self._env.reset()
         steps = 0
-        # print()
 
+        # Game loop
         while(True):
             steps+=1
 
-            if self.render:
-                self.env.render()
+            if self._render:
+                self._env.render()
 
+            # Have the agent take an action in the game
             action = self.choose_action(state)
-            new_state, reward, done, info = self.env.step(action)
-            # print(new_state)
-
-            # increase reward for keeping the cart in the center
-            # if abs(new_state[0]) < 1:
-            #     reward += 1
+            new_state, reward, done, info = self._env.step(action)
 
             # increase reward for keeping the pole strait
             if abs(new_state[2]) < .05:
@@ -108,14 +114,16 @@ class GameRunner:
             elif abs(new_state[2]) < 1.5:
                 reward += 1
 
-            if done:
+            if done or steps == 200:
                 new_state = None
 
-            self.memory.add_sample((state, action, reward, new_state))
+            # After each action add it to memory and train the model
+            self._memory.add_sample((state, action, reward, new_state))
             self.train()
 
+            # Exponentially decay the epsilon factor
             self._steps += 1
-            self.eps = MIN_EPS + (MAX_EPS - MIN_EPS) \
+            self._eps = MIN_EPS + (MAX_EPS - MIN_EPS) \
                                       * math.exp(-LAMBDA * self._steps)
 
             state = new_state
@@ -123,45 +131,68 @@ class GameRunner:
             if(steps > self._best_run):
                 self._best_run = steps
 
-            # if(steps > 200):
-            #     print('done {}'.format(done))
-
-            if done:
+            if done or steps == 200:
                 break
 
         return steps
 
+    # Train the model with a batch of data
     def train(self):
-        batch = self.memory.get_samples(self.model.batch_size)
+        # Get data from memory
+        batch = self._memory.get_samples(self._model.batch_size)
+
+        # Seperate out the state and new states from data
         states = np.array([i[0] for i in batch])
-        new_states = np.array([(np.zeros(self.model.num_states)
+        new_states = np.array([(np.zeros(self._model.num_states)
                                  if val[3] is None else val[3]) for val in batch])
 
-        q_old = self.model.predict_batch(states)
-        q_new = self.model.predict_batch(new_states)
+        # From the states get the Q values
+        q_old = self._model.predict_batch(states)
+        q_new = self._model.predict_batch(new_states)
 
-        x = np.zeros((self.model.batch_size, self.model.num_states))
-        y = np.zeros((self.model.batch_size, self.model.num_actions))
+        # np array to hold the training data
+        x = np.zeros((self._model.batch_size, self._model.num_states))
+        y = np.zeros((self._model.batch_size, self._model.num_actions))
 
         for i, b in enumerate(batch):
             state, action, reward, new_state = b[0], b[1], b[2], b[3]
 
+            # Q value for each data sample
             q = q_old[i]
 
+            # Update Q value of the action taken based on future rewards
+            # if the game is over don't
             if new_state is None:
                 q[action] = reward
             else:
                 q[action] = reward + GAMMA * np.amax(q_new[i])
 
+            # add values to training data
             x[i] = state
             y[i] = q
-        self.model.train_batch(x, y)
+
+        self._model.train_batch(x, y)
 
     def choose_action(self, state):
-        if random.random() < self.eps:
+        # Choose random action on a chance based on epsilon
+        if random.random() < self._eps:
             return random.randint(0,1)
+        # Have the model choose the action
         else:
-            return np.argmax(self.model.predict(state))
+            return np.argmax(self._model.predict(state))
+
+    # Show the model playing the game
+    def playGame(self):
+        steps = 0
+        while(True):
+            state = self._env.reset()
+            steps +=1
+            self._env.render()
+            action = self.choose_action(state)
+            state, reward, done, info = self._env.step(action)
+
+            if done or steps == 200:
+                break
 
     @property
     def best_run(self):
@@ -170,6 +201,7 @@ class GameRunner:
 if __name__ == "__main__":
     env = gym.make("CartPole-v1")
 
+    # Get number of states and actions from envirnment
     num_states = env.env.observation_space.shape[0]
     num_actions = env.env.action_space.n
 
@@ -179,12 +211,13 @@ if __name__ == "__main__":
     runner = GameRunner(model, env, memory, MAX_EPS, MIN_EPS, LAMBDA, render=False)
 
     num_games = 300
-
     completed = 0
     for i in range(num_games):
         steps = runner.run()
-        if steps >= 200:
+
+        if steps == 200:
             completed +=1
+        # Display number of games that reach max length every 50 games
         if (i+1)%50 == 0:
             print('Games {} - {}, {} games completed.'.format(i-48, i+1, completed))
             completed = 0
