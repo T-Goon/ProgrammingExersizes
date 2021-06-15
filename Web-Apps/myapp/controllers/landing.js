61cb4abb6ca69c2b542e321ca5cad0a938e37f84
Timothy Goon <t.kgoonx@gmail.com>
2021-06-15 17:29:58 -0400

    can now edit lead emails

--- Web-Apps/myapp/controllers/landing.js
+++ Web-Apps/myapp/controllers/landing.js
@@ -15,7 +15,8 @@ exports.submit_lead = async function(req, res, next) {
     const client = await pool.pool.connect();
 
     const result = await client.query('INSERT INTO leads (createdOn, updatedOn, email) \
-    VALUES ('+ 'NOW()::date' + ', '
+    VALUES ('
+    + 'NOW()::date' + ', '
     + 'NOW()::date' + ', '+
      '\''+req.body.lead_email+'\');');
     client.release();
@@ -53,10 +54,49 @@ exports.show_lead = async function(req, res, next) {
   try{
     const client = await pool.pool.connect();
 
-    const result = await client.query('SELECT * FROM leads WHERE id='+req.params.lead_id+';');
-    console.log(result)
+    const result = await client.query('SELECT * FROM leads WHERE id=\''+req.params.lead_id+'\';');
 
     res.render('lead', { lead: result.rows[0] });
+
+    client.release();
+  } catch (err){
+
+    console.error(err);
+    res.send("Error "+err)
+  }
+}
+
+// Show edit form for a lead
+exports.show_edit_lead = async function(req, res, next) {
+
+  // Get all leads
+  try{
+    const client = await pool.pool.connect();
+
+    const result = await client.query('SELECT * FROM leads WHERE id=\''+req.params.lead_id+'\';');
+
+    res.render('lead/edit_lead', { lead: result.rows[0] });
+
+    client.release();
+  } catch (err){
+
+    console.error(err);
+    res.send("Error "+err)
+  }
+}
+
+// Edits a lead
+exports.edit_lead = async function(req, res, next) {
+  // req.params.lead_id - from path
+  // req.body.lead_email - from html page
+
+  // Get all leads
+  try{
+    const client = await pool.pool.connect();
+
+    const result = await client.query('UPDATE leads SET email=\''+req.body.lead_email+'\' WHERE id=\''+req.params.lead_id+'\';');
+
+    res.redirect('/lead/'+ req.params.lead_id);
 
     client.release();
   } catch (err){
