b42aa5aec2542aebc719b489a02d2664b9203f0c
Timothy Goon <t.kgoonx@gmail.com>
2021-06-17 17:17:46 -0400

    added function stubs and some dependencies

--- Web-Apps/myapp/controllers/user.js
+++ Web-Apps/myapp/controllers/user.js
@@ -7,3 +7,7 @@ exports.show_login = function(req, res, next) {
 exports.show_signup = function(req, res, next) {
     res.render('user/signup', { title: 'Express', formData: {}, errors: {} });
 }
+
+exports.show_signup = function(req, res, next) {
+    
+}
